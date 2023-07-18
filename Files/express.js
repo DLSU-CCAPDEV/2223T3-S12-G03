f20438ff7fc99695593e0e1aa40bc61cf9917a91
Allen Neo Merto <129864230+nine-4@users.noreply.github.com>
2023-07-18 15:51:25 +0800

    added express.js, routes, and 404.hbs

---
+++ Files/express.js
@@ -0,0 +1,51 @@
+/*
+Use in the terminal when there is no node_modules folder:
+    npm install
+*/
+
+const dotenv = require("dotenv")
+dotenv.config()
+port = process.env.PORT
+hostname = process.env.HOSTNAME
+
+const express = require("express")
+
+const hbs = require("hbs")
+
+const routes = require("./routes/routes.js")
+
+// const db = require("./models/db.js")
+
+const app = express()
+
+// partials
+hbs.registerPartials(__dirname, "/views/partials")
+
+// view engine
+app.set("view engine", "hbs")
+
+//body parser
+app.use(express.urlencoded({extended: true}))
+
+// static files
+app.use(express.static("public"))
+
+
+
+// routes for the webpages
+app.use("/", routes)
+
+// if route not found, load default error webpage in ../views/404.hbs
+app.use(function (req, res) {
+    res.render('404.hbs');
+});
+
+
+
+// connects to the database
+// db.connect()
+
+app.listen(port, hostname, function(){
+    console.log("Server running at: ")
+    console.log("http://" + hostname + ":" + port)
+})
---
+++ Files/routes/routes.js
@@ -0,0 +1,21 @@
+const express = require("express")
+
+//const controller = require("../controllers/controller.js")
+
+//const post_controller = require("../controllers/post_controller.js")
+
+//const search_controller = require("../controllers/search_controller.js")
+
+//const signup_controller = require("../controllers/signup_controller.js")
+
+// const validation = require("../helpers/validation.js")
+
+const app = express()
+
+
+app.get("/", function(req, res){
+    res.sendFile("index.html")
+})
+
+
+module.exports = app
